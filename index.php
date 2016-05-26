899acbafdec5505495aa6cefdb615ba980f060e2
William Souto <william35bobs@gmail.com>
2016-05-26 12:48:19 -0300

    Protótipo do site, primeiros componentes e cabeçalho do site.

---
+++ index.php
@@ -0,0 +1,88 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Portal do Ensino</title>
+    <link rel="stylesheet"  href="./web/css/isset/bootstrap.min.css">
+    <link rel="stylesheet"  href="./web/css/isset/font-awesome.min.css">
+    <link rel="stylesheet"  href="./web/css/isset/components.css">
+    <link rel="stylesheet"  href="./web/css/index/head.css">
+    <script type="text/javascript" src="./web/js/isset/jquery-2.2.3.min.js"></script>
+    <script type="text/javascript" src="./web/js/isset/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="topo">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-xs-6">
+                    <div class="email">
+                        <i class="fa fa-envelope color-blue" aria-hidden="true"></i>
+                        <a>william35bobs@gmail.com</a>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-xs-6">
+                    <div class="row">
+                        <div class="col-lg-10 col-xs-10">
+                            <div class="rede-social">
+                                <div class="btn-group pull-right">
+                                    <a class="btn color-blue" target="_blank" title="Portal do ensino"  href="http://www.facebook.com.br/bobs.souto">
+                                        <i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
+                                    </a>
+                                    <a class="btn">
+                                        <i class="fa fa-google-plus-square fa-2x" aria-hidden="true"></i>
+                                    </a>
+                                    <a class="btn">
+                                        <i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-xs-2">
+                            <div class="login pull-right">
+                                <a href="">
+                                    <i class="fa fa-user color-blue fa-lg" aria-hidden="true"></i>
+                                    <span>login</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="menu">
+        <div class="col-lg-3 col-xs-3">
+            <img src="logo.png">
+        </div>
+        <div class="col-lg-9 col-xs-9">
+            <ul class="menu-bar">
+                <li class="bg-blue">Disciplinas</li>
+                <li class="bg-red">Video Aula</li>
+                <li class="bg-yellow">Método de Ensino</li>
+                <li class="bg-green">Sobre Nós</li>
+            </ul>
+        </div>
+    </div>
+    <div class="container-portal">
+
+        <div class="container">
+            <div class="titulo">
+                <h3>Portal do Ensino</h3>
+                <span>um jeito fácil de ensinar<span>
+            </div>
+            <div class="panel-pricing panel-pricing-default">
+                <div class="row">
+                    <div class="col-lg-4 col-xs-4">
+
+                    </div>
+                    <div class="col-lg-8 col-xs-8">
+                        <span class="disciplina bg-blue"><i class="fa fa-stethoscope" aria-hidden="true"></i></span>Biologia
+                        <span class="disciplina"><i class="fa fa-heart" aria-hidden="true"></i></span>Química
+                        <span class="disciplina"><i class="fa fa-plus-square" aria-hidden="true"></i></span>Física
+                        <span class="disciplina"><i class="fa fa-ambulance" aria-hidden="true"></i></span><Matemática></Matemática>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
